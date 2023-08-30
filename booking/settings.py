d4f17d676938ce78d62ba99bec3b1b23f12de249
Mohammed Ziyad <ziyad.szakeer@gmail.com>
2023-08-30 17:41:30 +0530

    main changes

--- booking/settings.py
+++ booking/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = 'staticfiles/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 MEDIA_URL = '/media/'
